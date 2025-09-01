dea83dbabe0e9749237b13dfd1dce189ac3d2bde
Jayaram <109912693+jayarampogakula@users.noreply.github.com>
2025-09-01 15:53:14 +0530

    Create vite.config.js

---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",   // build output folder
+  },
+  server: {
+    port: 5173,       // dev server port
+  }
+})
